f1108a5287a108f82c85a029f1a292681d3c4a75
sagar <sagar.tukrul@gamil.com>
2024-03-14 21:16:02 +0530

    Change end points of the url

--- WMS_Server.py
+++ WMS_Server.py
@@ -57,30 +57,28 @@ except:ShowPopup('Error!!!', "Database not connected.")
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST','GET'])
-def Main():
+@app.route('/putaway', methods=['Get', 'Post'])
+def putaway():
     global cursor
 
     if request.method == 'POST':
 
         content = request.json
+        response = ""
+        Status = ""
 
-        
-        if content['Process'] == "Putaway":
-            response = ""
-            Status = ""
-
-            Matrial_Code = content["Barcode"]
-            Putaway_Location = content['Location']
-            try:
-                cursor.execute(f"select Status, Location from tblPOC_Stock where Barcode='{Matrial_Code}'")
-                dbData = cursor.fetchall()
-                Matcode = Matrial_Code.split('|')
+        Matrial_Barcode = content["Barcode"]
+        Putaway_Location = content['Location']
+        try:
+            cursor.execute(f"select Status, Location from tblPOC_Stock where Barcode='{Matrial_Barcode}'")
+            dbData = cursor.fetchall()
+            Matcode = Matrial_Barcode.split('|')
+            if len(dbData) > 0:
                 for x in dbData:
                     if x[0] == 1: 
-                        cursor.execute(f"update tblPOC_Stock set Status = 2, Location ='{Putaway_Location}' where Barcode = '{Matrial_Code}'")
-                        cursor.execute(f"update tblPOC_Import_Data set Status = 2 where Mat_Code = '{Matcode[0]}'")
-                        cursor.execute(f"update tblPOC_Pick set Status = 2 where MatCode = '{Matcode[0]}'")
+                        cursor.execute(f"update tblPOC_Stock set Status = 2, Location ='{Putaway_Location}' where Barcode = '{Matrial_Barcode}'")
+                        # cursor.execute(f"update tblPOC_Import_Data set Status = 2 where Mat_Code = '{Matcode[0]}'")
+                        cursor.execute(f"update tblPOC_Pick set Status = 2 where MatCode = '{Matcode[0]}' and Batch = '{Matcode[1]}'")
                         cursor.commit()
                         
 
@@ -91,34 +89,59 @@ def Main():
                         response = "Already Putaway complete"
                     else:
                         Status = "Unsuccessful"
-                        response = "Please complete in process"
-            except Exception as ex: 
+                        response = "Please complete Putaway process"
+            else:
                 Status = "Error"
-                response = "Got Error, Please try again"
-                print(ex)
+                response = "Barcode not found"
+        except Exception as ex: 
+            print(ex)
+            Status = "Error"
+            response = "Got Error, Please try again"
 
-            context = {
-                "Status" : Status,
-                "Message" : response
-            }
-            return json.dumps(context)
+        context = {
+            "Status" : Status,
+            "Message" : response
+        }
+        return json.dumps(context)
         
-        elif content['Process'] == "GetDeliveryList":
-            cursor.execute(f"select DISTINCT  Delivery from tblPOC_Pick")
+@app.route('/pickup', methods=['Get', 'Post'])
+def pickup():
+        global cursor
+        Status = ""
+        response = ""
+
+        try:
+            cursor.execute(f"select Delivery, MatCode, Batch, Req_Box_Qty from tblPOC_Pick where Status = 2")
             exc_data = cursor.fetchall()
-            # print(exc_data)
-            temp = []
-            for x in exc_data: 
-                temp.append(x[0])
+            DeliveryList = []
+            if len(exc_data) > 0:
+                for x in exc_data: 
+                    temp = []
+                    temp.append(x[0])
+                    temp.append(x[1])
+                    temp.append(x[2])
+                    temp.append(x[3])
+                    DeliveryList.append(temp)
+                Status = "Successful"
+                response = DeliveryList
+            else:
+                Status = "Unsuccessful"
+                response = "Delivery list not found"
 
-            context = {
-                "Delivery_list":temp
-            }
-            return json.dumps(context)
+        except Exception as ex: 
+            print(ex)
+            Status = "Error"
+            response = "Got Error, Please try again"
 
+        context = {
+            "Response":response,
+            "Status" : Status
+        }
+        return json.dumps(context)
 
-    elif request.method == 'GET':
-        return "<h1> WMS Server is running </h1>"
+@app.route("/", methods=['POST','GET'])
+def Main():
+    return "<h1> WMS Server is running </h1>"
 
 
 
